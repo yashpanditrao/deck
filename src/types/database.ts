b4b1076ada1c766619547bab461024f2578ef613
Manthan Maheshwari <mmanthan3009@gmail.com>
2025-09-20 17:45:13 +0530

    types changes

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,7 +1,7 @@
 export interface DeckShareLink {
   id: string;
   deck_url: string;
-  comapany_id: string;
+  company_id: string;
   recipient_email: string;
   token: string;
   verification_code: string | null;
